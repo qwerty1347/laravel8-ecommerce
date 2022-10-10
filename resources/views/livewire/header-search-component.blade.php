80f32af1bbb67e3fe96f550cef61d62066119ac3
onch-kunhee <kunhee.lee@onch3.co.kr>
2022-10-10 21:43:18 +0900

    [수정] 메인페이지 상품 검색 기능 수정 #1

--- resources/views/livewire/header-search-component.blade.php
+++ resources/views/livewire/header-search-component.blade.php
@@ -1,7 +1,7 @@
 <div class="wrap-search center-section">
     <div class="wrap-search-form">
         <form action="{{ route('product.search') }}" id="form-search-top" name="form-search-top">
-            <input type="text" name="search" value="" placeholder="Search here...">
+            <input type="text" name="search" value="{{ $search }}" placeholder="Search here...">
             <button form="form-search-top" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
             <div class="wrap-list-cate">
                 <input type="hidden" name="product_cat" value="{{ $product_cat }}" id="product-cate">
