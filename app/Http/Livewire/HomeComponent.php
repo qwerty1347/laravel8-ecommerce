4b8f66cab6ef70d2f71b72102e0b2eedc2119dea
onch-kunhee <kunhee.lee@onch3.co.kr>
2022-11-13 16:32:52 +0900

    [추가] 메인페이지 세일 탭메뉴 기능

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -18,7 +18,8 @@ class HomeComponent extends Component
         $cats = explode(',', $category->sel_categories);
         $categories = Category::whereIn('id', $cats)->get();
         $no_of_products = $category->no_of_products;
+        $sproducts = Product::where('sale_price', '>', 0)->inRandomOrder()->get()->take(8);
 
-        return view('livewire.home-component', compact('sliders', 'lproducts', 'categories', 'no_of_products'))->layout('layouts.base');
+        return view('livewire.home-component', compact('sliders', 'lproducts', 'categories', 'no_of_products', 'sproducts'))->layout('layouts.base');
     }
 }
